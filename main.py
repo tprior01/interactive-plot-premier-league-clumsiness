d604f8be2afff971c614899a7e20db9d286f6191
tprior01 <tprior01@student.bbk.ac.uk>
2022-06-15 10:07:19 +0100

    update

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def updatebar():
                         data[directory].append(0)
         seasonal.data = data
         q.x_range.factors = seasonal.data['seasons']
-        q.title = '%s mistakes by season' % highlight_name.value
+        q.title.text = '%s mistakes by season' % highlight_name.value
 
 def updatescatter():
     df = select_players()
