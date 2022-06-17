f6596c5598e2a58df6c1798fbdaf4a53a3c2016d
tprior01 <tprior01@student.bbk.ac.uk>
2022-06-17 10:54:21 +0100

    update

--- main.py
+++ main.py
@@ -158,16 +158,20 @@ def updatesize():
     p.title.text = '%d players selected' % size
 
 def goalkeeper(attr, old, new):
-    print(f'old: {}, new: {}', old, new)
+    print(old)
+    print(new)
     highlight_name.value = position_data['Goalkeeper'].data['name'].iloc[new[0]]
 def defender(attr, old, new):
-    print(f'old: {}, new: {}', old, new)
+    print(old)
+    print(new)
     highlight_name.value = position_data['Defender'].data['name'].iloc[new[0]]
 def midfielder(attr, old, new):
-    print(f'old: {}, new: {}', old, new)
+    print(old)
+    print(new)
     highlight_name.value = position_data['Midfielder'].data['name'].iloc[new[0]]
 def forward(attr, old, new):
-    print(f'old: {}, new: {}', old, new)
+    print(old)
+    print(new)
     highlight_name.value = position_data['Forward'].data['name'].iloc[new[0]]
 
 renderers[0].data_source.selected.on_change('indices', goalkeeper)
