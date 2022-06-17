eea49cee0355680d2aa6c6ed84435a027167c4e5
tprior01 <tprior01@student.bbk.ac.uk>
2022-06-17 11:09:50 +0100

    update

--- main.py
+++ main.py
@@ -158,18 +158,22 @@ def updatesize():
     p.title.text = '%d players selected' % size
 
 def goalkeeper(attr, old, new):
+    print(attr)
     print(old)
     print(new)
     highlight_name.value = position_data['Goalkeeper'].data['name'].iloc[new[0]]
 def defender(attr, old, new):
+    print(attr)
     print(old)
     print(new)
     highlight_name.value = position_data['Defender'].data['name'].iloc[new[0]]
 def midfielder(attr, old, new):
+    print(attr)
     print(old)
     print(new)
     highlight_name.value = position_data['Midfielder'].data['name'].iloc[new[0]]
 def forward(attr, old, new):
+    print(attr)
     print(old)
     print(new)
     highlight_name.value = position_data['Forward'].data['name'].iloc[new[0]]
